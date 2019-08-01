d11d6ec56df8006446ed235bcb80b89dd8c12380
isaacddr <isaacovic00@gmail.com>
2019-08-01 18:31:23 -0500

    added unifi api

--- index.py
+++ index.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from ubiquiti.unifi import UNIFI_API as UNIFI
+from sys import argv
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def hello_world():
-    return 'Hello, World!'
 
+    unifi_user = []
 
+    with UNIFI(username="admin", password="Smhau$31.%", site="default", baseurl="https://unifi.smarthaus.com.mx:8443", verify=False) as unifi:
+        for user in unifi.list_clients():
+            unifi_user.add(user)
+
+    return unifi_user
+    
